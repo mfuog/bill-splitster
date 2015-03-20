88a96adb258baf4726fac127803ce230ef857d6a
Myrtha Fuog <myrtha@fuog.net>
2015-03-20 16:23:10 +0100

    Fix typ conversion exception
    Fixes "no implicit conversion of Symbol into Integer"

--- app/models/bill_sheet.rb
+++ app/models/bill_sheet.rb
@@ -1,6 +1,6 @@
 class BillSheet < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
-  enum :status [:open, :closed]
+  enum status: [:open, :closed]
 
   validates :creator, presence: true
   validates :status, presence: true
