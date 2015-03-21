e1fff4397e63cab4d8120f5727130e2d11f75412
Myrtha Fuog <myrtha@fuog.net>
2015-03-21 16:41:48 +0100

    Remove participants and bills by links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,7 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
+}
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Helper method that:
+  # - sets a hidden field for the _destroy attribute's value to 1 so that the
+  #   record gets deleted when the surrounding form is submitted
+  # - sets a link that visually hides the form's fields.
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to(name, "#", onclick: "remove_fields(this)")
+  end
 end
