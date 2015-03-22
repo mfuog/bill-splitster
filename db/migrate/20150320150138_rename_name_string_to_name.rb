e733674bfa025ef366afabacdedcec5ccedf67a9
Myrtha Fuog <myrtha@fuog.net>
2015-03-22 08:30:44 +0100

    Fix typo

--- db/migrate/20150320150138_rename_name_string_to_name.rb
+++ db/migrate/20150320150138_rename_name_string_to_name.rb
@@ -1,6 +1,6 @@
 class RenameNameStringToName < ActiveRecord::Migration
   def change
-	# The name 'name_string' was due to a typo. 
+  # The name 'name_string' was due to a typo. 
   rename_column :participants, :name_string, :name
   end
 end
